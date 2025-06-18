8fe0ab97f2989bca7a5b8ce4b189472f3704aa34
Yasar <saminyasarsunny@gmail.com>
2025-06-19 00:24:19 +0600

    Add file

---
+++ git.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int v;
+     printf("enter the value\n");
+     scanf("%d", &v);
+     
+printf("%d * 1 = %d\n",v,v*1);
+printf("%d * 2 = %d\n",v,v*2);
+printf("%d * 3 = %d\n",v,v*3);
+printf("%d * 4 = %d\n",v,v*4);
+printf("%d * 5 = %d\n",v,v*5);
+printf("%d * 6 = %d\n",v,v*6);
+printf("%d * 7 = %d\n",v,v*7);
+printf("%d * 8 = %d\n",v,v*8);
+printf("%d * 9 = %d\n",v,v*9);
+printf("%d * 10 = %d\n",v,v*10);
+
+    return 0;
+}
