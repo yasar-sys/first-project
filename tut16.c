a0f2d355d80e5b778d0916b3b350cb67e0bb6b8e
Yasar <saminyasarsunny@gmail.com>
2025-07-10 22:59:04 +0600

    tutorial16

---
+++ tut16.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,age;
+
+    for (i = 0; i<5; i++)
+    {
+        printf("%d\nEnter your age\n", i);
+        scanf("%d", &age);
+
+        if (age>10)
+        {
+            continue;
+        }
+
+        printf("sunny is a bad boy");
+
+        
+    
+    }
+
+
+    return 0;
+}    
