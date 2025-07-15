e7759ee2c51610e2af20a32c7a0db1de6815f5fb
Yasar <saminyasarsunny@gmail.com>
2025-07-15 16:10:57 +0600

    tut18

---
+++ tut18.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 3;
+  float b = (float) 34/3;
+
+  printf("the value of a is %d\n",a);
+  printf("the value of b is %f\n",b);
+
+
+
+
+
+
+
+  return 0;
+}
