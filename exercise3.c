5d2b8ba3f109571f4321e33ae802ef72e3eb7412
yasar <saminyasarsunny@gmail.com>
2025-08-01 05:23:13 +0600

    exer3

--- exercise3.c
+++ exercise3.c
@@ -1,43 +1,48 @@
-// #include <stdio.h>
-// int fibanocci(int n)
-// {
-// if (n == 1 || n == 2)
-// {
-//     return 1;
-// }
-// else
-// {
-//     return (fibanocci(n - 1) + fibanocci(n - 2));
-// }
-// }
-// int main()
-// {
-//     int number;
-//     printf("enter the number you want to the fibanocci number\n");
-//     scanf("%d", &number);
-//     printf("the value of the fibanocci of %d is %d \n", number, fibanocci(number));
-//     return 0;
-// }
-
-
 #include <stdio.h>
-
+int fibanocci(int n)
+{
+    if (n == 1 || n == 2)
+    {
+        return n - 1;
+    }
+    else
+    {
+        return (fibanocci(n - 1) + fibanocci(n - 2));
+    }
+}
 int main()
 {
-    int a;
-    int t1 = 0;
-    int t2 = 1;
-    int next = t1 + t2;
-    
-    printf("Enter the number till which you want the Fibonacci series : ");
-    scanf("%d", &a);
-    printf("The Fibonacci series are %d",next);
-    for (int i = 3; i <= a; i++)
-    {
-        printf(" %d", next);
-        t1 = t2;
-        t2 = next;
-        next = t1 + t2;
-    }
+    int number;
+    printf("enter the number you want to the fibanocci number\n");
+    scanf("%d", &number);
+    printf("the value of the fibanocci of %d is %d \n", number, fibanocci(number));
     return 0;
 }
+
+// #include <stdio.h>
+
+// int iteritive(int n)
+// {
+
+//     int a = 0;
+//     int b = 1;
+
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         b = a + b;
+//         a = b - a;
+//     }
+//     return a;
+// }
+
+// int main()
+// {
+
+//     int num;
+
+//     printf("Enter the number till which you want the Fibonacci series : ");
+//     scanf("%d", &num);
+//     printf("the value of the fibanocci of %d is %d \n", num, iteritive(num));
+
+//     return 0;
+// }
